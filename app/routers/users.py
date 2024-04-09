f9f43830ff9110629d0bbd083ee55d6caa9db44e
tbotalla <tbotalla@gmail.com>
2024-04-08 21:06:50 -0300

    fiubyte/.github#16: Added query parameter 'mine' on GET /services for filtering current authenticated user's services
    + Also added approved field for User entity

--- app/routers/users.py
+++ app/routers/users.py
@@ -57,7 +57,7 @@ def create_user(
     if not user_to_upsert:
         # Insert: no previous user
         user_to_upsert = User(email=user.email, password=hashed_pwd, roles=role_to_create, name=user.name,
-                              surname=user.surname,
+                              surname=user.surname, approved=False,
                               profile_photo_url=user.profile_photo_url, document_number=user.document_number,
                               address=user.address,
                               address_lat=address_lat, address_long=address_long, max_radius=user.max_radius,
